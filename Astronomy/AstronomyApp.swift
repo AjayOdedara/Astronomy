34b5230d3cbaba74c69c2a9d239b92001a62de7b
Ajay Odedara <ajodedra16@gmail.com>
2026-04-26 12:41:42 +0100

    Initial Commit

---
+++ Astronomy/AstronomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyApp.swift
+//  Astronomy
+//
+//  Created by Ajay Odedara on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
